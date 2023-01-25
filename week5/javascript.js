8c69ef794ca185b8cdc4a01a7ad8e634b88fa676
Danni Raphael Morta <drmorta@student.apc.edu.ph>
2023-01-25 23:24:33 +0800

    Lesson 15.5 - Added Comments on JavaScript code

--- week5/javascript.js
+++ week5/javascript.js
@@ -1,14 +1,18 @@
+//Date Function
 const d = new Date();
 
+//New Window
 function newWindow() {
       var myWindow = window.open("", "", "width=200, height=100");
       myWindow.blur();
 	  };
+//Object (me)
 const me = {
         firstName: "Danni Raphael",
         lastName: "Morta",
         age: 20,
       };
+//Random Number Generator
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
     };
