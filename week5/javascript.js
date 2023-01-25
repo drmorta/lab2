0a63ec396afa5bf0211003a21972de901cb385bf
Danni Raphael Morta <drmorta@student.apc.edu.ph>
2023-01-25 18:08:05 +0800

    Lesson 17.1 - Added an object JS Code

---
+++ week5/javascript.js
@@ -0,0 +1,12 @@
+const d = new Date();
+
+function newWindow() {
+      var myWindow = window.open("", "", "width=200, height=100");
+      myWindow.blur();
+	  };
+const me = {
+        firstName: "Danni Raphael",
+        lastName: "Morta",
+        age: 20,
+        eyeColor: "blue"
+      };
