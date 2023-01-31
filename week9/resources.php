98aae77e65eec79c25edffb39012ad586bd268eb
Danni Raphael Morta <drmorta@student.apc.edu.ph>
2023-01-31 09:17:52 +0800

    Updated Repository

---
+++ week9/resources.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+</head>
+<body>
+<a href = "https://www.w3schools.com/cssref/pr_background-position.php">W3 Schools CSS Course</a>
+<a href = "https://www.w3schools.com/js/default.asp">W3 Schools JavaScript Course</a>
+</body
+</html>
