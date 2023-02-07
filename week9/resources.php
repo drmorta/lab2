1e96468c0dd0aa54033bb7b4272dad6c51e76abc
Danni Raphael Morta <119904010+drmorta@users.noreply.github.com>
2023-02-07 21:13:55 +0800

    Delete week9 directory

--- week9/resources.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-</head>
-<body>
-<a href = "https://www.w3schools.com/cssref/pr_background-position.php">W3 Schools CSS Course</a>
-<a href = "https://www.w3schools.com/js/default.asp">W3 Schools JavaScript Course</a>
-</body
-</html>
